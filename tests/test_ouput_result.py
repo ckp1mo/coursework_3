a9320867ba83380e3619e2dae1471e1aabe36b19
Alexandr Gritsunov <ickpimo@gmail.com>
2023-09-03 12:50:06 +0300

    initial

---
+++ tests/test_ouput_result.py
@@ -0,0 +1,42 @@
+import pytest
+
+from utils.output_result import print_result
+
+
+file = [
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }, {
+    "id": 596171168,
+    "state": "EXECUTED",
+    "date": "2018-07-11T02:26:18.671407",
+    "operationAmount": {
+      "amount": "79931.03",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 72082042523231456215"
+  }
+]
+
+
+def test_print_result():
+    assert print_result(file) == None
+    assert print_result('') is None
+    with pytest.raises(TypeError):
+        assert print_result([1, 2, 3])
---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+import pytest
+from utils.utils import load_operation_json, sort_date
+from utils.utils import get_5_executed_operations
+
+test_json = r'tests\test_json.json'
+date = ["2019-08-26T10:50:58.294041", "2019-07-03T18:35:29.512364"]
+
+
+def test_load_operation_json():
+    with pytest.raises(FileNotFoundError):
+        assert load_operation_json('')
+        assert load_operation_json(test_json) == test_json
+
+
+def test_sort_date():
+    assert sort_date('') == []
+    assert sort_date(load_operation_json(test_json)) == ['2019-08-26T10:50:58.294041', '2019-07-03T18:35:29.512364']
+    assert sort_date(['h', 'l', 'y']) == []
+    with pytest.raises(TypeError):
+        assert sort_date([1, 2, 3])
+
+
+#
+#
+def test_get_5_executed_operations():
+    assert get_5_executed_operations(load_operation_json(test_json),
+                                     sort_date(load_operation_json(test_json))) == load_operation_json(test_json)
+    with pytest.raises(AssertionError):
+        assert get_5_executed_operations(' ', ' ')
